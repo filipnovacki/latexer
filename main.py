6830fdddfcc61a23338b3e548fcb8e0fb6a6de52
Filip Novački <fnovacki@pm.me>
2018-09-13 20:11:22 +0200

    Bug while parsing arguments, added changes to latexer script

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # This script generater LaTeX files
 
 import argparse
-import sys
 from Logic.Compositor import Compositor
 
 parser = argparse.ArgumentParser(description='Start a LaTeX file. Allows a lot of default customization'
@@ -37,15 +36,6 @@ parser.add_argument('-lang',
 parser.add_argument('title',
                     help='Title of the paper')
 
-if len(sys.argv) == 1:
-    parser.print_help(sys.stderr)
-    sys.exit(1)
-
-# comp = Compositor()
 args = parser.parse_args()
 comp = Compositor(args)
-# print(args)
 
-# All data is saved in comp class instance
-# print(comp.font_size)
-# print(file_content)
